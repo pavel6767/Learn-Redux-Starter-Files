79e5ab2f3fc92dc58cd163738c1497699812f271
Pavel Machuca <pavelmachuca@hotmail.com>
2018-12-21 15:03:55 -0500

    done vid2

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+
+import css from './styles/style.styl';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
